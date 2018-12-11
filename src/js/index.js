842d61e996bb00f4c924b6e5666c92c1000beebc
Diadumenos <diadumenos@Diadumenoss-MacBook-Pro.local>
2018-12-12 01:01:59 +0200

    added animations

--- src/js/index.js
+++ src/js/index.js
@@ -1,33 +1,37 @@
-import $ from 'jquery';
-import Scrollbar from 'smooth-scrollbar';
-import home from './modules/home';
-import contacts from './modules/contacts';
-import thinking from './modules/thinking';
+import $ from 'jquery'
+import Scrollbar from 'smooth-scrollbar'
+// import WOW from 'wowjs'
+import home from './modules/home'
+import contacts from './modules/contacts'
+import thinking from './modules/thinking'
 
-
-
-
-
+$(document).ready(() => {})
 var app = {
-	init : function(){
-		var page = $('body').attr('class');        
-		Scrollbar.init(document.querySelector('#main-scroll'));
-		switch (page) {
-  			case 'home':
-    			home();    
-    		break;
-  			case 'contacts':
-    			contacts();
-    		break;
-        case 'thinking':
-          thinking();
-        break;
-  			default:
-    	// statements_def
-    		break;
-		}
-	}	
+  init: function() {
+    if (document.querySelector('.wow')) {
+      window.wow = new WOW.WOW({
+        live: false
+      })
+
+      window.wow.init()
+    }
+    var page = $('body').attr('class')
+    Scrollbar.init(document.querySelector('#main-scroll'))
+    switch (page) {
+      case 'home':
+        home()
+        break
+      case 'contacts':
+        contacts()
+        break
+      case 'thinking':
+        thinking()
+        break
+      default:
+        // statements_def
+        break
+    }
+  }
 }
 
-
-app.init();
+app.init()
