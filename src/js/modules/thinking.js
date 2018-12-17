c21b14405255960e621333280129fa3a4c901972
ScooterOk <scooterok@gmail.com>
2018-12-17 11:32:17 +0200

    mobile

--- src/js/modules/thinking.js
+++ src/js/modules/thinking.js
@@ -1,7 +1,9 @@
 import datePicker from 'air-datepicker';
 import en from 'air-datepicker/dist/js/i18n/datepicker.en.js';
-export default function thinking() {	
+export default function thinking() {
+	var position;	
 	$('#datepicker').datepicker({
-    language: 'en'
-  });  
+	    language: 'en',
+	    position : $(window).width() > 480 ? 'bottom left' : 'bottom right'
+	  });  
 };
